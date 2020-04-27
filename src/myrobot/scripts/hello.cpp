34f2dca3ba70b53e04fc0f1cebb127007a997a55
stephen derosa <stephen.d.derosa@nasa.gov>
2020-04-27 09:14:32 -0500

    file renames, final submission world, cheat sheet

--- src/myrobot/scripts/hello.cpp
+++ src/myrobot/scripts/hello.cpp
@@ -6,7 +6,7 @@ namespace gazebo
   {
     public: WorldPluginMyRobot() : WorldPlugin()
             {
-              printf("Hello World!\n");
+              printf("Hello and welcome to the one, the ONLY, World of the Quarantined!\n");
             }
 
     public: void Load(physics::WorldPtr _world, sdf::ElementPtr _sdf)
